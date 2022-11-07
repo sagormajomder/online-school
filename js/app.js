ab7deed93ca9f0c4489e45708540b860ba8b5391
sagormajomder <sagormajomder@gmail.com>
2022-11-07 12:49:59 +0600

    sticky menu done

--- js/app.js
+++ js/app.js
@@ -12,4 +12,25 @@ const mainFunction = function () {
    btnMobile.addEventListener("click", function () {
       header.classList.toggle("nav-open");
    });
+
+   // Sticky Navigation
+   const sectionHeroEl = document.querySelector(".hero-section");
+
+   const obs = new IntersectionObserver(
+      function (entries) {
+         const ent = entries[0];
+         if (ent.isIntersecting === false) {
+            document.body.classList.add("sticky");
+         }
+         if (ent.isIntersecting) {
+            document.body.classList.remove("sticky");
+         }
+      },
+      {
+         root: null,
+         threshold: 0,
+         rootMargin: "-74px",
+      }
+   );
+   obs.observe(sectionHeroEl);
 };
